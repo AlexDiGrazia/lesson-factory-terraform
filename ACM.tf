fa5776572ab4934fa79e63723e0abb5fb6e99850
Alex Digrazia <alexndigrazia@hotmail.com>
2024-11-28 17:08:01 -0800

    changed protocol to provision SSL certificate through AWS console because it wasn't working via terraform.  Accordingly, changed Terraform code to provision HTTPS LB listener using SSL certificate ARN generated through AWS console.

--- ACM.tf
+++ ACM.tf
@@ -1,14 +1,14 @@
-resource "aws_acm_certificate" "lf_certificate" {
-  domain_name       = "www.lesson-factory.com"
-  validation_method = "DNS"
+# resource "aws_acm_certificate" "lf_certificate" {
+#   domain_name       = "www.lesson-factory.com"
+#   validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_acm_certificate_validation" "acm_cert_validation" {
-  certificate_arn         = aws_acm_certificate.lf_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.lf_record : record.fqdn]
-  depends_on = [ aws_acm_certificate.lf_certificate ]
-}
+# resource "aws_acm_certificate_validation" "acm_cert_validation" {
+#   certificate_arn         = aws_acm_certificate.lf_certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.lf_record : record.fqdn]
+#   depends_on = [ aws_acm_certificate.lf_certificate ]
+# }
--- route53.tf
+++ route53.tf
@@ -1,27 +1,27 @@
-resource "aws_route53_zone" "primary" {
-  name = "www.lesson-factory.com"
-}
+# resource "aws_route53_zone" "primary" {
+#   name = "www.lesson-factory.com"
+# }
 
-data "aws_route53_zone" "primary_data" {
-  name         = aws_route53_zone.primary.name
-  private_zone = false
-  depends_on = [ aws_route53_zone.primary ]
-}
+# data "aws_route53_zone" "primary_data" {
+#   name         = aws_route53_zone.primary.name
+#   private_zone = false
+#   depends_on = [ aws_route53_zone.primary ]
+# }
 
-resource "aws_route53_record" "lf_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.lf_certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "lf_record" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.lf_certificate.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 300
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.primary_data.zone_id
-  depends_on = [ aws_acm_certificate.lf_certificate ]
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 300
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.primary_data.zone_id
+#   depends_on = [ aws_acm_certificate.lf_certificate ]
+# }
